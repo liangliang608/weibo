cc7ddc1a9f55665e1924149b8acdc15ffa9944c4
liangliang608 <liangliang608@gmail.com>
2020-10-22 11:28:55 +0000

    完成用户注册功能’

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,14 +8,15 @@
   <title>@yield('title', 'Weibo App')</title>
   <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
+
 <body>
     @include('layouts._header')
     <div class="container">
       <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
           @yield('content')
         @include('layouts._footer')
       </div>
     </div>
-
 </body>
 </html>
