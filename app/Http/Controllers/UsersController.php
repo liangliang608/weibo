154ddec03c561a79db31196a91dfba6e3381d97e
liangliang608 <liangliang608@gmail.com>
2020-10-22 19:07:32 +0000

    登录成功后的导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,23 +7,31 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    //用户注册
     public function create(){
         return view('users.create');
     }
+    //个人信息页面
     public  function show(User $user) {
         return view('users.show', compact('user'));
     }
+
     public function store(Request $request){
+        //验证用户信息
         $this->validate($request,[
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
+        //创建用户
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+        //自动登录
+        Auth::login($user);
+        //登录成功
         session()->flash('success','欢迎，您将在这里开启一段新的旅程');
         return redirect()->route('users.show',[$user]);
     }
