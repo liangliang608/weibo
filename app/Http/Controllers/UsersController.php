1110ead9d5c33662d47df471da0fa527b441c19d
liangliang608 <liangliang608@gmail.com>
2020-10-23 17:52:34 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
 {
     public function __construct() {
         $this->middleware('auth', [
-            'except' =>['show', 'create','store']
+            'except' =>['show', 'create','store','index']
         ]);
         $this->middleware('guest', [
             'only'=>['create']
@@ -43,8 +43,14 @@ class UsersController extends Controller
         //登录成功
         session()->flash('success','欢迎，您将在这里开启一段新的旅程');
         return redirect()->route('users.show',[$user]);
-
     }
+
+    //用户列表
+    public  function index() {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
     public function edit(User $user) {
         $this->authorize('update', $user);
         return view('users.edit', compact('user'));
