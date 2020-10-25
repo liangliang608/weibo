c20628b44d755c1484e619ce53aaaf5c144bf93a
liangliang608 <liangliang608@gmail.com>
2020-10-25 13:09:23 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -75,5 +75,10 @@ class UsersController extends Controller
 //        ]);
         return redirect()->route('users.show', $user);
     }
+    public function destroy(User  $user) {
+        $user->delete();
+        session()->flash('success','成功删除用户');
+        return back();
+    }
 
 }
